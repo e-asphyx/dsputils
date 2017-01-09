73b2da5619818d2a0b7fb272a7b1363b9648a803
Eugene Zagidullin <e.asphyx@gmail.com>
2017-01-09 20:29:43 +0200

    Quantize

--- coef.go
+++ coef.go
@@ -1,8 +1,17 @@
 package dsputils
 
+import (
+	"math"
+)
+
 type FilterCoef struct {
 	A []float64
 	B []float64
+}
+
+type IntFilterCoef struct {
+	A []int64
+	B []int64
 }
 
 func (f FilterCoef) Norm() FilterCoef {
@@ -13,6 +22,36 @@ func (f FilterCoef) Norm() FilterCoef {
 
 	return FilterCoef{
 		A: f.A,
+		B: b,
+	}
+}
+
+func round(x float64) float64 {
+	if x >= 0 {
+		return math.Trunc(x + 0.5)
+	} else {
+		return math.Trunc(x - 0.5)
+	}
+}
+
+func quantize(val float64, bits uint) int64 {
+	return int64(round(val * float64(int64(1)<<bits)))
+}
+
+func (f FilterCoef) Quantize(bits uint) IntFilterCoef {
+	b := make([]int64, len(f.B))
+	a := make([]int64, len(f.A))
+
+	for i, val := range f.B {
+		b[i] = quantize(val, bits)
+	}
+
+	for i, val := range f.A {
+		a[i] = quantize(val, bits)
+	}
+
+	return IntFilterCoef{
+		A: a,
 		B: b,
 	}
 }
